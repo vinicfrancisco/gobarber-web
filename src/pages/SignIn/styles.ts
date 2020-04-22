b2f766eaeea0c029784b7040be08f42acd93063a
vinicfrancisco <vini.ibi@gmail.com>
2020-04-22 09:09:37 -0300

    create signin page layout

---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,98 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+import signinBackground from '../../assets/signin-background.png';
+
+export const Container = styled.div`
+  align-items: stretch;
+  display: flex;
+  height: 100vh;
+`;
+
+export const Content = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 700px;
+  width: 100%;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    input {
+      background: #232129;
+      border-radius: 10px;
+      border: 2px solid #232129;
+      color: #f4ede8;
+      padding: 16px;
+      width: 100%;
+
+      &::placeholder {
+        color: #666360;
+      }
+
+      & + input {
+        margin-top: 8px;
+      }
+    }
+
+    button {
+      background: #ff9000;
+      border: 0;
+      border-radius: 10px;
+      color: #312e38;
+      font-weight: 500;
+      height: 56px;
+      margin-top: 16px;
+      padding: 0 16px;
+      transition: background-color 0.2s;
+      width: 100%;
+
+      &:hover {
+        background: ${shade(0.2, '#ff9000')};
+      }
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+
+  > a {
+    align-items: center;
+    color: #ff9000;
+    display: flex;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    > svg {
+      margin-right: 16px;
+    }
+
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
+  }
+`;
+
+export const Background = styled.div`
+  background: url(${signinBackground}) no-repeat center;
+  background-size: cover;
+  flex: 1;
+`;
