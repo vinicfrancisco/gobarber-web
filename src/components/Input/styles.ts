ce10ef8225a59a12ee40b866fd935b041ea7350a
vinicfrancisco <vini.ibi@gmail.com>
2020-04-22 09:35:45 -0300

    created button and input global components

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  align-items: center;
+  background: #232129;
+  border-radius: 10px;
+  border: 2px solid #232129;
+  color: #666360;
+  display: flex;
+  padding: 16px;
+  width: 100%;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  input {
+    background: transparent;
+    border: 0;
+    color: #f4ede8;
+    flex: 1;
+
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`;
