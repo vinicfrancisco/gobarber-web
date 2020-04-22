7c01930e245f12e6b7fc5f9c456e4687684156ca
vinicfrancisco <vini.ibi@gmail.com>
2020-04-22 17:09:05 -0300

    fixed toast component

---
+++ src/components/ToastContainer/Toast/index.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+import {
+  FiAlertCircle,
+  FiXCircle,
+  FiCheckCircle,
+  FiInfo,
+} from 'react-icons/fi';
+
+import { useToast, ToastMessage } from '../../../hooks/toast';
+
+import { Container } from './styles';
+
+interface ToastProps {
+  toast: ToastMessage;
+}
+
+const icons = {
+  info: <FiInfo size={24} />,
+  error: <FiAlertCircle size={24} />,
+  success: <FiCheckCircle size={24} />,
+};
+
+const Toast: React.FC<ToastProps> = ({ toast }) => {
+  const { removeToast } = useToast();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(toast.id);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [toast.id, removeToast]);
+
+  return (
+    <Container
+      key={toast.id}
+      type={toast.type}
+      hasDescription={!!toast.description}
+    >
+      {icons[toast.type || 'info']}
+
+      <div>
+        <strong>{toast.title}</strong>
+        {toast.description && <p>{toast.description}</p>}
+      </div>
+
+      <button type="button" onClick={() => removeToast(toast.id)}>
+        <FiXCircle size={18} />
+      </button>
+    </Container>
+  );
+};
+
+export default Toast;
--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,49 +1,21 @@
 import React from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
 
-import { Container, Toast } from './styles';
+import { ToastMessage } from '../../hooks/toast';
 
-const ToastContainer: React.FC = () => {
+import Toast from './Toast';
+
+import { Container } from './styles';
+
+interface ToastContainerProps {
+  messages: ToastMessage[];
+}
+
+const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
   return (
     <Container>
-      <Toast hasDescription>
-        <FiAlertCircle size={20} />
-
-        <div>
-          <strong>erro</strong>
-
-          <p>nao foi possivel fazer login</p>
-        </div>
-
-        <button type="button">
-          <FiXCircle size={18} />
-        </button>
-      </Toast>
-
-      <Toast type="error" hasDescription={false}>
-        <FiAlertCircle size={20} />
-
-        <div>
-          <strong>erro</strong>
-        </div>
-
-        <button type="button">
-          <FiXCircle size={18} />
-        </button>
-      </Toast>
-      <Toast type="success" hasDescription>
-        <FiAlertCircle size={20} />
-
-        <div>
-          <strong>erro</strong>
-
-          <p>nao foi possivel fazer login</p>
-        </div>
-
-        <button type="button">
-          <FiXCircle size={18} />
-        </button>
-      </Toast>
+      {messages.map((message) => (
+        <Toast key={message.id} toast={message} />
+      ))}
     </Container>
   );
 };
