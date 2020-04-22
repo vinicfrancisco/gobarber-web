e61093630241209aebe6e97416aa925f377de973
vinicfrancisco <vini.ibi@gmail.com>
2020-04-22 16:21:34 -0300

    created toast layout

---
+++ src/components/ToastContainer/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
+
+import { Container, Toast } from './styles';
+
+const ToastContainer: React.FC = () => {
+  return (
+    <Container>
+      <Toast hasDescription>
+        <FiAlertCircle size={20} />
+
+        <div>
+          <strong>erro</strong>
+
+          <p>nao foi possivel fazer login</p>
+        </div>
+
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+
+      <Toast type="error" hasDescription={false}>
+        <FiAlertCircle size={20} />
+
+        <div>
+          <strong>erro</strong>
+        </div>
+
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+      <Toast type="success" hasDescription>
+        <FiAlertCircle size={20} />
+
+        <div>
+          <strong>erro</strong>
+
+          <p>nao foi possivel fazer login</p>
+        </div>
+
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+    </Container>
+  );
+};
+
+export default ToastContainer;
